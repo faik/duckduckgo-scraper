094721642d2c16a83aa4f838f2d8180b288d3dee
Faik Uygur <faik@robomotion.io>
2026-02-21 19:42:00 +0000

    Update flow from Robomotion

--- main.ts
+++ main.ts
@@ -1,92 +1,26 @@
-import { flow, Message, Custom } from '@robomotion/sdk';
+import { flow, Custom, Message } from '@robomotion/sdk';
 
-const FLOW_ID = 'b1cc3fe3-241e-40fd-8b75-af47a38b6b9b';
-
-const duckDuckGoFlow = flow.create(FLOW_ID, 'DuckDuckGo Scraper', (f) => {
-    f.addDependency('Core.Trigger', '26.1.0');
-    f.addDependency('Core.Dialog', '26.1.0');
-    f.addDependency('Core.Browser', '26.1.0');
-    f.addDependency('Core.Excel', '26.1.0');
-    f.addDependency('Core.Flow', '26.1.0');
-    f.addDependency('Core.Programming', '26.1.0');
-
-    f.node('dd39a8', 'Core.Trigger.Inject', 'Start', {})
-        .then('d25a94', 'Core.Dialog.InputBox', 'Get Query', {
-            inTitle: Custom('DuckDuckGo Search'),
-            inText: Custom('Enter your search query:'),
-            outText: Message('query')
-        })
-        .then('d3fbf4', 'Core.Browser.Open', 'Open Browser', {
-            optBrowser: 'chrome',
-            optMaximized: true,
-            outBrowserId: Message('browser_id')
-        })
-        .then('92c2b0', 'Core.Browser.OpenLink', 'Navigate', {
-            inBrowserId: Message('browser_id'),
-            inUrl: Custom('https://duckduckgo.com'),
-            outPageId: Message('page_id')
-        })
-        .then('9f9204', 'Core.Browser.TypeText', 'Type Query', {
-            inPageId: Message('page_id'),
-            inSelector: Custom('//*[@id="searchbox_input"]'),
-            inSelectorType: 'xpath:position',
-            inText: Message('query')
-        })
-        .then('f4a190', 'Core.Browser.ClickElement', 'Search', {
-            inPageId: Message('page_id'),
-            inSelector: Custom('//body/div/div/div/main/article/div[1]/div[1]/div[2]/div/header/div/section[2]/div/form/div/div[3]/button'),
-            inSelectorType: 'xpath:position'
-        })
-        .then('aebce4', 'Core.Browser.WaitElement', 'Wait for Results', {
-            inPageId: Message('page_id'),
-            inSelector: Custom('article[data-testid="result"]'),
-            inSelectorType: 'css',
-            optTimeout: Custom('10')
-        })
-        .then('cbdffc', 'Core.Browser.RunScript', 'Scrape Results', {
-            inPageId: Message('page_id'),
-            func: `
-                var results = [];
-                var elements = document.querySelectorAll('article[data-testid="result"]');
-                elements.forEach(function(el) {
-                    var titleEl = el.querySelector('h2 a');
-                    if (titleEl) {
-                        results.push({
-                            Title: titleEl.innerText,
-                            Link: titleEl.href
-                        });
-                    }
-                });
-                return JSON.stringify({ columns: ['Title', 'Link'], rows: results });
-            `,
-            outResult: Message('table_json')
-        })
-        .then('a4d044', 'Core.Programming.Function', 'Parse Results', {
-            func: `
-                msg.table = JSON.parse(msg.table_json);
-                msg.excel_path = global.get('$Home$') + '/results.xlsx';
-                return msg;
-            `
-        })
-        .then('b8306c', 'Core.Excel.Create', 'Create Excel', {
-            inPath: Message('excel_path'),
-            optOverwrite: true,
-            outFileDescriptor: Message('excel_fd')
-        })
-        .then('5a7688', 'Core.Excel.SetRange', 'Write Results', {
-            inFileDescriptor: Message('excel_fd'),
-            inStartCell: Custom('A1'),
-            inTable: Message('table'),
-            optHeader: true,
-            optTarget: 'specific-cell'
-        })
-        .then('f12128', 'Core.Excel.Save', 'Save Excel', {
-            inFileDescriptor: Message('excel_fd')
-        })
-        .then('9528f0', 'Core.Browser.Close', 'Close Browser', {
-            inBrowserId: Message('browser_id')
-        })
-        .then('9262e8', 'Core.Flow.Stop', 'Stop', {});
-});
-
-duckDuckGoFlow.start();
+flow.create('f212efc9-113b-4d06-8732-ada8e0ab2c99', 'Duck Duck External', (f) => {
+  f.node('dd39a8', 'Core.Trigger.Inject', 'Start', {})
+    .then('d25a94', 'Core.Dialog.InputBox', 'Get Query', {
+    inTitle: Custom('DuckDuckGo Search'),
+    inText: Custom('Enter your search query:'),
+    outText: Message('query')
+  })
+    .then('d3fbf4', 'Core.Browser.Open', 'Open Browser', {})
+    .then('92c2b0', 'Core.Browser.OpenLink', 'Navigate', { inUrl: Custom('https://duckduckgo.com') })
+    .then('9f9204', 'Core.Browser.TypeText', 'Type Query', { inSelector: Custom('//*[@id="searchbox_input"]'), inText: Message('query') })
+    .then('f4a190', 'Core.Browser.ClickElement', 'Search', { inSelector: Custom('//body/div/div/div/main/article/div[1]/div[1]/div[2]/div/header/div/section[2]/div/form/div/div[3]/button') })
+    .then('aebce4', 'Core.Browser.WaitElement', 'Wait for Results', {
+    inSelectorType: 'css',
+    inSelector: Custom('article[data-testid="result"]'),
+    optTimeout: Custom('10')
+  })
+    .then('cbdffc', 'Core.Browser.RunScript', 'Scrape Results', { func: '\n                var results = [];\n                var elements = document.querySelectorAll(\'article[data-testid="result"]\');\n                elements.forEach(function(el) {\n                    var titleEl = el.querySelector(\'h2 a\');\n                    if (titleEl) {\n                        results.push({\n                            Title: titleEl.innerText,\n                            Link: titleEl.href\n                        });\n                    }\n                });\n                return JSON.stringify({ columns: [\'Title\', \'Link\'], rows: results });\n            ', outResult: Message('table_json') })
+    .then('a4d044', 'Core.Programming.Function', 'Parse Results', { func: '\n                msg.table = JSON.parse(msg.table_json);\n                msg.excel_path = global.get(\'$Home$\') + \'/results.xlsx\';\n                return msg;\n            ' })
+    .then('b8306c', 'Core.Excel.Create', 'Create Excel', { inPath: Message('excel_path'), optOverwrite: true })
+    .then('5a7688', 'Core.Excel.SetRange', 'Write Results', {})
+    .then('f12128', 'Core.Excel.Save', 'Save Excel', {})
+    .then('9528f0', 'Core.Browser.Close', 'Close Browser', {})
+    .then('9262e8', 'Core.Flow.Stop', 'Stop', {});
+}).start();
